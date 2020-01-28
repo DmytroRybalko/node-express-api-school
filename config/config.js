31163a186ef45ebff150c214a85406931148cb8f
DmytroR <my_email@gmail.com>
2020-01-28 19:36:31 +0200

    fix config.js by adding module.exports =

--- config/config.js
+++ config/config.js
@@ -1,12 +1,11 @@
-{
+module.exports = {
   "development": {
     "username": "me",
     "password": "password",
     "database": "high_school",
     "host": "127.0.0.1",
     "dialect": "postgres",
-    "port": 5432,
-    "operatorsAliases": false
+    "port": 5432
   },
   "test": {
     "username": "root",
